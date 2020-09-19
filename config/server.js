f6c471d45791c2193571d1ae7a01791f6513e768
Ayrton <ayrtonpgt@gmail.com>
2020-09-19 18:05:28 +0200

    backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '99627d08ab83ef22f64f4b644a5787b2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd0bf196f-fe29-480d-b3f5-a40bcf493531'
+};
